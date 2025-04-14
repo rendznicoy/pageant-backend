213a3f6344bc04e5fd19e74f98bbbd5810dff934
rendznicoy <rendzdelosreyes@gmail.com>
2025-04-14 09:31:43 +0800

    [CORS] - Set up CORS and Cookie Auth.

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,11 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    'credentials' => true,
+
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +31,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
