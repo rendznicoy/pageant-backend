b30be42a2c269c6d847158786f93a384dc2f3c87
rendznicoy <rendzdelosreyes@gmail.com>
2025-05-28 20:28:40 +0800

    [FINAL] [VERSION 0.2.6]

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
-// Sanctum CSRF cookie route - this is the standard Sanctum route
+// Sanctum CSRF cookie route
 Route::get('/sanctum/csrf-cookie', function () {
     return response()->json(['message' => 'CSRF cookie set']);
 });
 
-// You can also add a custom route if needed
+// Alternative CSRF route if needed
 Route::get('/api/csrf-cookie', function () {
     return response()->json(['message' => 'CSRF cookie set']);
 });
 
-// Your other web routes...
 Route::get('/', function () {
     return response()->json(['message' => 'API is running']);
 });
